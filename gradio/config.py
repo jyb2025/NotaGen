ddea3dbce27ac7306cc4a43d906b532618b7b916
jady2025 <jybwolf@163.com>
2026-03-04 23:38:06 +0800

    Update inference weights path in config.py

--- gradio/config.py
+++ gradio/config.py
@@ -1,7 +1,8 @@
+# config.py
 import os
 
 # Configurations for inference
-INFERENCE_WEIGHTS_PATH = 'weights_notagenx_p_size_16_p_length_1024_p_layers_20_h_size_1280.pth'               # Path to weights for inference# Folder to save output files
+INFERENCE_WEIGHTS_PATH = 'weights_notagen_finetune3_p_size_16_p_length_1024_p_layers_20_c_layers_6_h_size_1280_lr_5e-06_batch_4.pth'               # Path to weights for inference# Folder to save output files
 TOP_K = 9                                                       # Top k for sampling
 TOP_P = 0.9                                                      # Top p for sampling
 TEMPERATURE = 1.2                                                 # Temperature for sampling
@@ -13,3 +14,4 @@ PATCH_LENGTH = 1024                                             # Patch Length
 CHAR_NUM_LAYERS = 6                                             # Number of layers in the decoder
 PATCH_NUM_LAYERS = 20                                           # Number of layers in the encoder
 HIDDEN_SIZE = 1280                                               # Hidden Size
+
